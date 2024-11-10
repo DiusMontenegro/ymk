c5c18b07f6296a180171216f21b3a162b9896c46
Dius Montenegro <montenegrodiussantos1@gmail.com>
2024-11-10 20:39:06 +0800

    style: Tablet view Feature Section Protection and Defense

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -99,40 +99,39 @@ const Home = () => (
         </section>
 
         {/* Feature Section - Protection and Defense */}
-        <section className="relative h-[80vh] lg:h-[91vh]">
+        <section className="relative h-[80vh] md:h-[50vh] lg:h-[91vh]">
             <div className="absolute left-0 top-0 h-1/2 w-full bg-[#ffd6d8]"></div>
             <div className="absolute bottom-0 left-0 h-1/2 w-full bg-[#fafafa]"></div>
             <div className="flex-center container relative mx-auto h-full">
-                <h3 className="font-bodoni-moda absolute left-0 top-0 text-[80px] font-bold leading-none text-white lg:text-[230px]">
+                <h3 className="font-bodoni-moda absolute left-0 top-0 text-[80px] font-bold leading-none text-white md:text-[180px] lg:text-[230px]">
                     YMK
                 </h3>
-                <h3 className="font-bodoni-moda absolute bottom-0 right-0 text-[80px] font-bold leading-none text-black opacity-10 lg:text-[230px]">
+                <h3 className="font-bodoni-moda absolute bottom-0 right-0 text-[80px] font-bold leading-none text-black opacity-10 md:text-[180px] lg:text-[230px]">
                     YMK
                 </h3>
-                {/* TODO: design md viewport 768x1024 */}
-                <div className="z-10 flex h-[480px] w-[270px] flex-col bg-[#fafafa] text-[#916618] shadow-lg md:flex-row-reverse">
+                <div className="z-10 flex h-[480px] w-[270px] flex-col bg-[#fafafa] text-[#916618] shadow-lg md:h-[350px] md:w-[600px] md:flex-row-reverse">
                     {/* <--- EDIT HERE ---> */}
                     <Image
-                        className="h-1/2 flex-1 object-cover object-top"
+                        className="h-1/2 flex-1 object-cover object-top md:h-full md:w-1/2 md:object-cover md:object-right"
                         src="/assets/protection-poster.webp"
                         alt="skin-protection-poster"
                         width={2000}
                         height={2000}
                     />
                     <div className="flex-1 px-4 py-6 text-center">
-                        <h4 className="font-bodoni-moda mb-3 text-xl font-semibold tracking-wider">
+                        <h4 className="font-bodoni-moda mb-3 text-xl font-semibold tracking-wider md:text-3xl">
                             Protection & Defense
                         </h4>
-                        <p className="font-roboto mb-6 text-[12px]">
+                        <p className="font-roboto mb-6 text-[12px] md:px-3 md:text-justify md:text-[14px] md:tracking-normal">
                             SPF 30+++ and Fullerene RS Vitamin C offers comprehensive protection
                             against environmental damage and aging. SPF 30+++ shields the skin from
-                            UVA and UVB rays, preventing sun damage and premature aging.{" "}
+                            UVA and UVB rays, preventing sun damage and premature aging.
                         </p>
                         <Button
                             size="sm"
-                            className="mx-auto h-[30px] w-[65px] rounded-sm border border-[#916618] text-[#916618] hover:bg-[#bb9044] hover:text-[#fafafa]"
+                            className="mx-auto h-[30px] w-[75px] rounded-sm border border-[#916618] font-semibold text-[#916618] hover:bg-[#bb9044] hover:text-[#fafafa] md:h-[35px] md:w-[85px] md:text-[14px]"
                             variant="outline">
-                            Buy Now
+                            Read More
                         </Button>
                     </div>
                 </div>
